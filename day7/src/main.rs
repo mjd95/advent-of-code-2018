172c3d14829425bc8cdce0971ca98bac44a37213
Martin Dickson <martin.dickson34@gmail.com>
2018-12-14 07:58:56 +0000

    day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::hash::{Hash, Hasher};
 use std::io::{self, BufReader};
 use std::env;
 use std::io::prelude::*;
@@ -12,7 +11,6 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(&f);
 
     let mut deps: Vec<(String, String)> = Vec::new();
-    let mut completed: HashMap<String, bool> = HashMap::new();
 
     for line in reader.lines() {
         match line {
@@ -24,35 +22,93 @@ fn main() -> io::Result<()> {
         }
     }
 
-    let mut to_complete: Vec<String> = Vec::new();
+    let mut to_start: Vec<String> = Vec::new();
     for dep in &deps {
         let (s, t) = dep;
-        if !to_complete.contains(s) {
-            to_complete.push(s.to_string());
+        if !to_start.contains(s) {
+            to_start.push(s.to_string());
         }
-        if !to_complete.contains(t) {
-            to_complete.push(t.to_string());
+        if !to_start.contains(t) {
+            to_start.push(t.to_string());
         }
     }
 
-    to_complete.sort();
-    while to_complete.len() > 0 {
-        for i in 0..to_complete.len() {
-            if canDo(i, &deps, &to_complete) {
-                print!("{}", to_complete[i]);
-                to_complete.remove(i);
-                break;
+    to_start.sort();
+
+    let base_time = 60;
+    let num_workers = 5;
+    let mut times: HashMap<i64, i64> = HashMap::new();
+    let mut tasks: HashMap<i64, String> = HashMap::new();
+    for i in 0..num_workers {
+        times.insert(i, 0);
+        tasks.insert(i, "".to_string());
+    }
+    let mut tick = 0;
+
+    let mut in_flight: Vec<String> = Vec::new();
+
+    while to_start.len() > 0 || in_flight.len() > 0 {
+        let mut i = 0;
+        while i < to_start.len() {
+            let mut delta = 1;
+            if can_do(i, &deps, &to_start) {
+                if not_blocked_by_inflight(to_start[i].clone(), &deps, &in_flight) {
+                    for (k, v) in times.iter_mut() {
+                        if *v == 0 {
+                            *v = base_time + (to_start[i].as_bytes()[0] as i64) - 64;
+                            tasks.insert(*k, to_start[i].clone());
+                            in_flight.push(to_start[i].clone());
+                            to_start.remove(i);
+                            delta = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            i += delta;
+        }
+
+        for (k, v) in times.iter_mut() {
+            if *v > 0 {
+                *v -= 1;
+            }
+
+            if *v == 0 {
+                for i in 0..in_flight.len() {
+                    if in_flight[i] == *tasks.get(k).unwrap() {
+                        in_flight.remove(i);
+                        break;
+                    }
+                }
+                print!("{}", tasks.get(k).unwrap());
+                tasks.insert(*k, "".to_string());
             }
         }
+
+        tick += 1;
+
     }
+
+    println!("");
+    println!("{}", tick);
 
     Ok(())
 }
 
-fn canDo(i: usize, deps: &Vec<(String, String)>, to_complete: &Vec<String>) -> bool {
+fn not_blocked_by_inflight(task: String, deps: &Vec<(String, String)>, in_flight: &Vec<String>) -> bool {
     for dep in deps {
         let (s, t) = dep;
-        if *t == to_complete[i] && to_complete.contains(s) {
+        if *t == task && in_flight.contains(s) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn can_do(i: usize, deps: &Vec<(String, String)>, to_start: &Vec<String>) -> bool {
+    for dep in deps {
+        let (s, t) = dep;
+        if *t == to_start[i] && to_start.contains(s) {
             return false;
         }
     }
